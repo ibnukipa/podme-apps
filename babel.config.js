7c6118ff522711b2d6a6ed3bade585e9766090b9
Ibnu Prayogi <ibnu.dev@gmail.com>
2021-02-06 09:50:15 +0700

    [0] preset for eslint, flow, and prettier

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          assets: './src/assets',
+          components: './src/components',
+          constants: './src/constants',
+          hooks: './src/hooks',
+          routes: './src/routes',
+          screens: './src/screens',
+          states: './src/states',
+          utils: './src/utils',
+        },
+      },
+    ],
+  ],
 };
