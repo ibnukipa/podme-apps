1866c08cc0f245e9132ad8b91809896b513c50ca
Ibnu Prayogi <ibnu.dev@gmail.com>
2021-02-07 10:28:37 +0700

    [1] some screen

---
+++ src/screens/Login.js
@@ -0,0 +1,16 @@
+/* @flow */
+
+import React from 'react';
+
+import Container from 'components/Container';
+import Text from 'components/Text';
+import Icon from 'components/Icon';
+
+const Login: () => React$Node = () => (
+  <Container>
+    <Text bolder>test</Text>
+    <Icon />
+  </Container>
+);
+
+export default Login;
